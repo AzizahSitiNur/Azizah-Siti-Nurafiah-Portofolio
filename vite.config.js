b38d1863dbb532679f394b380ca47ccdec01c100
AzizahSitiNur <azizahsitinurafiah@gmail.com>
2026-01-11 19:07:25 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portofolio/', // <-- SESUAIKAN DENGAN NAMA REPO DI GITHUB
   plugins: [
     react({
       babel: {
